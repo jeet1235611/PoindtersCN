5cdfcd2fe51930a10aa465097da3f3758cb2c202
acoustic_jeet <js.jitendra11@gmail.com>
2018-06-22 02:20:30 +0530

    Create pointers16.cpp

---
+++ pointers16.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int i = 10;
+        int *p = &i;
+
+        cout << sizeof(p) << endl;
+        cout << i << endl;
+        cout << *p << endl;
+
+        i++;
+
+        cout << i << endl;
+        cout << *p << endl;
+
+        int a = i; //ye bhi likh sakte h, int a = *p
+        a++;
+
+        cout << a << endl;
+        cout << i << endl;
+
+        i = 12;
+        cout << i << endl;
+        cout << *p << endl;
+        *p = 23;
+        cout << i << endl;
+        cout << *p << endl;
+
+        (*p)++;
+
+        cout << i << endl;
+        cout << *p << endl;
+
+}
